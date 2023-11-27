1a2a1f93b0758695334aad5279d724dde60cb8fa
vikas phougat <vikas.phougat@kalolytic.com>
2023-11-27 21:52:21 +0530

    dcewdc

--- src/app/deshboard copy/layout.tsx
+++ src/app/deshboard copy/layout.tsx
@@ -2,13 +2,11 @@
 export default function MeasureLayout(props: {
   children: React.ReactNode;
   mainsection: React.ReactNode;
-  sidebar: React.ReactNode;
 }) {
   return (
     <>
       <div>{props.children}</div>
       <div>{props.mainsection}</div>
-      <div>{props.sidebar}</div>
     </>
   );
 }
